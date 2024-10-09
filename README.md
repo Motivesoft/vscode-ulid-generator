f03d44819ce4acd44fcc20970b43c87331dd7d4d
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-10-09 22:35:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# ULID Generator README
+
+This extension generates Universally Unique Lexicographically Sortable Identifiers, known as ULID.
+
+These have various advantages over other forms of unique identifiers, such as UUID, and are useful when it is desirable to have identifiers that can be meaningfully sorted.
+
+A comparison of the benefits of ULID over UUID and others can be found in the [canonical ULID spec](https://github.com/ulid/spec).
+
+## Features
+
+From the command palette, context menu or via a hotkey, one of more ULID can be placed into the current editor.
+
+## Requirements
+
+...
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: Enable/disable this extension.
+* `myExtension.thing`: Set to `blah` to do something.
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+---
+
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
+## Working with Markdown
+
+You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
+
+## For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ dist/extension.js
@@ -0,0 +1,330 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.activate = activate;
+exports.deactivate = deactivate;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = __importStar(__webpack_require__(1));
+const ulid_1 = __webpack_require__(2);
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+function activate(context) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('"vscode-ulid-generator" is now active');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    context.subscriptions.push(vscode.commands.registerCommand('vscode-ulid-generator.insert', () => {
+        // Generate the ULID and insert it at the current edit point
+        let editor = vscode.window.activeTextEditor;
+        if (editor) {
+            editor.edit(edit => {
+                editor?.selections.forEach(v => edit.replace(v, makeUlid()));
+            }).then(success => {
+                var select = vscode.workspace.getConfiguration().get("vscode-ulid-generator.textSelection");
+                if (success && editor && !select) {
+                    editor.selection = new vscode.Selection(editor.selection.end, editor.selection.end);
+                }
+            });
+        }
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('vscode-ulid-generator.copy', () => {
+        // Generate UUID and add it to the clipboard
+        vscode.env.clipboard.writeText(makeUlid());
+        // Display a message box to the user
+        vscode.window.showInformationMessage('ULID copied to clipboard');
+    }));
+}
+function makeUlid() {
+    return (0, ulid_1.ulid)();
+}
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+
+/***/ }),
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("vscode");
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   decodeTime: () => (/* binding */ decodeTime),
+/* harmony export */   detectPrng: () => (/* binding */ detectPrng),
+/* harmony export */   encodeRandom: () => (/* binding */ encodeRandom),
+/* harmony export */   encodeTime: () => (/* binding */ encodeTime),
+/* harmony export */   factory: () => (/* binding */ factory),
+/* harmony export */   incrementBase32: () => (/* binding */ incrementBase32),
+/* harmony export */   monotonicFactory: () => (/* binding */ monotonicFactory),
+/* harmony export */   randomChar: () => (/* binding */ randomChar),
+/* harmony export */   replaceCharAt: () => (/* binding */ replaceCharAt),
+/* harmony export */   ulid: () => (/* binding */ ulid)
+/* harmony export */ });
+function createError(message) {
+    var err = new Error(message);
+    err.source = "ulid";
+    return err;
+}
+// These values should NEVER change. If
+// they do, we're no longer making ulids!
+var ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"; // Crockford's Base32
+var ENCODING_LEN = ENCODING.length;
+var TIME_MAX = Math.pow(2, 48) - 1;
+var TIME_LEN = 10;
+var RANDOM_LEN = 16;
+function replaceCharAt(str, index, char) {
+    if (index > str.length - 1) {
+        return str;
+    }
+    return str.substr(0, index) + char + str.substr(index + 1);
+}
+function incrementBase32(str) {
+    var done = undefined;
+    var index = str.length;
+    var char = void 0;
+    var charIndex = void 0;
+    var maxCharIndex = ENCODING_LEN - 1;
+    while (!done && index-- >= 0) {
+        char = str[index];
+        charIndex = ENCODING.indexOf(char);
+        if (charIndex === -1) {
+            throw createError("incorrectly encoded string");
+        }
+        if (charIndex === maxCharIndex) {
+            str = replaceCharAt(str, index, ENCODING[0]);
+            continue;
+        }
+        done = replaceCharAt(str, index, ENCODING[charIndex + 1]);
+    }
+    if (typeof done === "string") {
+        return done;
+    }
+    throw createError("cannot increment this string");
+}
+function randomChar(prng) {
+    var rand = Math.floor(prng() * ENCODING_LEN);
+    if (rand === ENCODING_LEN) {
+        rand = ENCODING_LEN - 1;
+    }
+    return ENCODING.charAt(rand);
+}
+function encodeTime(now, len) {
+    if (isNaN(now)) {
+        throw new Error(now + " must be a number");
+    }
+    if (now > TIME_MAX) {
+        throw createError("cannot encode time greater than " + TIME_MAX);
+    }
+    if (now < 0) {
+        throw createError("time must be positive");
+    }
+    if (Number.isInteger(now) === false) {
+        throw createError("time must be an integer");
+    }
+    var mod = void 0;
+    var str = "";
+    for (; len > 0; len--) {
+        mod = now % ENCODING_LEN;
+        str = ENCODING.charAt(mod) + str;
+        now = (now - mod) / ENCODING_LEN;
+    }
+    return str;
+}
+function encodeRandom(len, prng) {
+    var str = "";
+    for (; len > 0; len--) {
+        str = randomChar(prng) + str;
+    }
+    return str;
+}
+function decodeTime(id) {
+    if (id.length !== TIME_LEN + RANDOM_LEN) {
+        throw createError("malformed ulid");
+    }
+    var time = id.substr(0, TIME_LEN).split("").reverse().reduce(function (carry, char, index) {
+        var encodingIndex = ENCODING.indexOf(char);
+        if (encodingIndex === -1) {
+            throw createError("invalid character found: " + char);
+        }
+        return carry += encodingIndex * Math.pow(ENCODING_LEN, index);
+    }, 0);
+    if (time > TIME_MAX) {
+        throw createError("malformed ulid, timestamp too large");
+    }
+    return time;
+}
+function detectPrng() {
+    var allowInsecure = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+    var root = arguments[1];
+
+    if (!root) {
+        root = typeof window !== "undefined" ? window : null;
+    }
+    var browserCrypto = root && (root.crypto || root.msCrypto);
+    if (browserCrypto) {
+        return function () {
+            var buffer = new Uint8Array(1);
+            browserCrypto.getRandomValues(buffer);
+            return buffer[0] / 0xff;
+        };
+    } else {
+        try {
+            var nodeCrypto = __webpack_require__(3);
+            return function () {
+                return nodeCrypto.randomBytes(1).readUInt8() / 0xff;
+            };
+        } catch (e) {}
+    }
+    if (allowInsecure) {
+        try {
+            console.error("secure crypto unusable, falling back to insecure Math.random()!");
+        } catch (e) {}
+        return function () {
+            return Math.random();
+        };
+    }
+    throw createError("secure crypto unusable, insecure Math.random not allowed");
+}
+function factory(currPrng) {
+    if (!currPrng) {
+        currPrng = detectPrng();
+    }
+    return function ulid(seedTime) {
+        if (isNaN(seedTime)) {
+            seedTime = Date.now();
+        }
+        return encodeTime(seedTime, TIME_LEN) + encodeRandom(RANDOM_LEN, currPrng);
+    };
+}
+function monotonicFactory(currPrng) {
+    if (!currPrng) {
+        currPrng = detectPrng();
+    }
+    var lastTime = 0;
+    var lastRandom = void 0;
+    return function ulid(seedTime) {
+        if (isNaN(seedTime)) {
+            seedTime = Date.now();
+        }
+        if (seedTime <= lastTime) {
+            var incrementedRandom = lastRandom = incrementBase32(lastRandom);
+            return encodeTime(lastTime, TIME_LEN) + incrementedRandom;
+        }
+        lastTime = seedTime;
+        var newRandom = lastRandom = encodeRandom(RANDOM_LEN, currPrng);
+        return encodeTime(seedTime, TIME_LEN) + newRandom;
+    };
+}
+var ulid = factory();
+
+
+
+
+/***/ }),
+/* 3 */
+/***/ ((module) => {
+
+module.exports = require("crypto");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__(0);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=extension.js.map
