561c286a48352db290975368e4a859d7752d382f
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-06-21 20:49:55 +0200

    Remove console output

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -11,14 +11,6 @@ const ulidFunction = ulid;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	if (vscode.env.uiKind === vscode.UIKind.Web) {
-		console.log("'vscode-ulid-generator' is active in an web extension host");
-	} else if (vscode.env.uiKind === vscode.UIKind.Desktop) {
-		console.log("'vscode-ulid-generator' is active in an desktop extension host");
-	} else {
-		console.log("'vscode-ulid-generator' is active in an unknown extension host");
-	}
-
 	context.subscriptions.push(vscode.commands.registerCommand('vscode-ulid-generator.insert', () => {
 		const seedTime = vscode.workspace.getConfiguration().get("vscode-ulid-generator.seedTime") as number;
 		const useMonotonic = vscode.workspace.getConfiguration().get("vscode-ulid-generator.monotonic") as boolean;
